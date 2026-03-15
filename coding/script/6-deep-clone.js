6e4540a3515b9d877785b06a655af359e57aa6a6
alfredchow2338 <alfredchow2338@gmail.com>
2026-03-15 23:23:20 +0800

    coding Q6 practice

--- coding/script/6-deep-clone.js
+++ coding/script/6-deep-clone.js
@@ -1,4 +1,4 @@
-const obj = [{
+const obj = {
   a: {
     b: {
       c: 1,
@@ -6,34 +6,20 @@ const obj = [{
     }
   },
   d: "string"
-}]
-
-obj[0].self = obj
+}
 
 const deepClone = (obj, map = new WeakMap()) => {
-  // primitive data type and null
-  if (obj === null || typeof obj !== 'object') {
+  if (typeof obj !== "object") {
     return obj
   }
 
-  if (map.has(obj)) {
-    return map.get(obj)
-  }
-
   if (obj instanceof Date) {
-    return new Date(obj);
-  }
-
-  if (Array.isArray(obj)) {
-    const clone = {}
-    map.set(obj, clone)
-    obj.forEach((key, idx) => {
-      clone[idx] = deepClone(obj[key], map)
-    })
+    return new Date(obj)
   }
 
   const clone = {}
   map.set(obj, clone)
+  console.log({map})
   for (const key in obj) {
     clone[key] = deepClone(obj[key], map)
   }
