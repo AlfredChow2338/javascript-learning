f9db33f5ab50a6ef4509f7f196c2f9c514b1b97a
alfredchow2338 <alfredchow2338@gmail.com>
2026-03-15 17:58:43 +0800

    coding Q8 practice

--- coding/script/8-flatten-map.js
+++ coding/script/8-flatten-map.js
@@ -5,6 +5,7 @@ const obj = {
   },
   class: {
     school: "cityu",
+    year: 4,
     major: {
       bsc: {
         dept: "sdsc"
@@ -13,58 +14,23 @@ const obj = {
   }
 };
 
-function flattenMap(obj, prefix = '', result = {}) {
-  // 處理 null 和 undefined
-  if (obj == null) {
-    if (prefix) {
-      result[prefix] = obj;
+function flattenMap(obj) {
+  const res = {}
+  const recursion = (ob, key) => {
+    if (typeof ob != 'object') {
+      return res[key] = ob
     }
-    return result;
+
+    Object.keys(ob).forEach(currKey => {
+      let newKey = key.length ? `${key}.${currKey}` : currKey
+      recursion(ob[currKey], newKey)
+    })
+
   }
-  
-  // 處理數組
-  if (Array.isArray(obj)) {
-    obj.forEach((item, index) => {
-      const key = prefix ? `${prefix}.${index}` : `${index}`;
-      flattenMap(item, key, result);
-    });
-    return result;
-  }
-  
-  // 處理對象
-  if (typeof obj === 'object') {
-    const keys = Object.keys(obj);
-    
-    if (keys.length === 0) {
-      // 空對象
-      if (prefix) {
-        result[prefix] = {};
-      }
-      return result;
-    }
-    
-    keys.forEach(key => {
-      const value = obj[key];
-      const newKey = prefix ? `${prefix}.${key}` : key;
-      
-      // 如果值是對象或數組，遞歸處理
-      if (value != null && typeof value === 'object') {
-        flattenMap(value, newKey, result);
-      } else {
-        // 基本類型，直接賦值
-        result[newKey] = value;
-      }
-    });
-    
-    return result;
-  }
-  
-  // 基本類型
-  if (prefix) {
-    result[prefix] = obj;
-  }
-  
-  return result;
+
+  recursion(obj, '')
+
+  return res
 }
 
 console.log(flattenMap(obj))
